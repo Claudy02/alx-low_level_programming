ba126d4d010be3ac283ccac1acf771f503d67edf
Ebuka Claude Nnanyelugo <claudennanyelugo@gmail.com>
2023-07-20 18:35:17 -0700

    messi

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+		putchar('\n');
+	for (a = 1; a <= size; a++)
+	{
+		for (b = 1; b <= size; b++)
+		{
+			if (b <= (size - a))
+				putchar(' ');
+			else
+				putchar('#');
+		}
+		putchar('\n');
+	}
+
+}
