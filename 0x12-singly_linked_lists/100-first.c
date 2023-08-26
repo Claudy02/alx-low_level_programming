388201eef2edfed6a68d1a47d13a97b483079c7c
Ebuka Claude Nnanyelugo <claudennanyelugo@gmail.com>
2023-08-25 21:41:44 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void preMainPrint(void)__attribute__((constructor));
+/**
+ * preMainPrint - prints a sentence before the main
+ * function is executed
+ */
+
+void preMainPrint(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+
+}
