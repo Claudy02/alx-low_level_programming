a8a4734541ea8b973954b292bfa4d2230276081a
Ebuka Claude Nnanyelugo <claudennanyelugo@gmail.com>
2023-08-29 16:25:06 -0700

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_listint - a function that prints all the
+ * elements in a list
+ * @h: struct listint_t type
+ * Return: size_t value
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
