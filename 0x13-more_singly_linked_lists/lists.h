6262feb2fcc4191c11e4b2a7dee04318236281ef
Ebuka Claude Nnanyelugo <claudennanyelugo@gmail.com>
2023-08-29 19:34:27 -0700

    u

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,6 +16,24 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
