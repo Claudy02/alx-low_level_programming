e79f66800b175dda528ec46f59d75452582e23c4
Ebuka Claude Nnanyelugo <claudennanyelugo@gmail.com>
2023-08-30 18:28:17 -0700

    up to date

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,4 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 /**
  * free_listint2 - frees a list but head is null
  * @head: linked list and head
@@ -9,15 +6,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *tmp;
 
 	if (head == NULL)
 		return;
 	while (*head != NULL)
 	{
-		temp = *head;
+		tmp = *head;
 		free(*head);
-		*head = temp->next;
+		*head = tmp->next;
 	}
 	*head = NULL;
 }
